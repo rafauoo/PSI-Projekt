ef89c0414634680e457afc29ed8de2e24c27b3c7
rafauoo <rafal5094@gmail.com>
2024-01-14 00:27:28 +0100

    debug

--- Tunnel-Server/tunnel_server.py
+++ Tunnel-Server/tunnel_server.py
@@ -74,7 +74,7 @@ def forward_tcp_connection(udp_socket, shared_dict, id):
 def start_udp_server(udp_socket, tcp_socket, shared_dict):
     while True:
         # Czekamy na pakiet UDP przychodzący od tunelu-serwera
-        udp_response, ret_address = udp_socket.recv(65535)
+        udp_response, ret_address = udp_socket.recvfrom(65535)
         # Odczytujemy ID Połączenia
         if not udp_response["conn_id"] in shared_dict.get_all_keys():
             connection, address = tcp_socket.connect((DESTINATION_SERVER, DESTINATION_PORT))
@@ -96,7 +96,7 @@ def main():
     udp_address_port = (TUNNEL_SERVER_IP, INSIDE_PORT)
 
     udp_socket = socket.socket(
-        family=socket.AF_INET, type=socket.SOCK_STREAM)
+        family=socket.AF_INET, type=socket.SOCK_DGRAM)
     udp_socket.bind(udp_address_port)
 
     print(f'Tunnel Server is up!')
