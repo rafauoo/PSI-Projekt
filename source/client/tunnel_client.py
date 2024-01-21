bd92cecf02a12630f53d34e61abcf99e919b0151
janekfilipecki <filipeckijan@gmail.com>
2024-01-21 15:41:10 +0100

    added args to server

--- source/client/tunnel_client.py
+++ source/client/tunnel_client.py
@@ -1,7 +1,8 @@
 import socket
 import argparse
 import threading
-from assets.msgtype import MsgType
+# from assets.msgtype import MsgType
+import enum
 import json
 
 TUNNEL_CLIENT_IP = '172.21.23.9'
@@ -9,6 +10,13 @@ OUTSIDE_PORT = 54321
 INSIDE_PORT = 12345
 TUNNEL_SERVER_IP = '172.21.23.10'
 TUNNEL_SERVER_PORT = 12345
+
+class MsgType(enum.Enum):
+    REQUEST = 1
+    RESPONSE = 2
+    INIT = 3
+    CONN_CLOSE_CLIENT = 4
+    CONN_CLOSE_SERVER = 5
 
 
 class SynchronizedDict:
--- source/server/tunnel_server.py
+++ source/server/tunnel_server.py
@@ -2,17 +2,25 @@ import socket
 import argparse
 import threading
 import json
-from assets.msgtype import MsgType
+# from assets.msgtype import MsgType
+import enum
+
+class MsgType(enum.Enum):
+    REQUEST = 1
+    RESPONSE = 2
+    INIT = 3
+    CONN_CLOSE_CLIENT = 4
+    CONN_CLOSE_SERVER = 5
 
 
-TUNNEL_CLIENT_IP = '172.21.23.9'
-TUNNEL_CLIENT_PORT = 12345
-TUNNEL_SERVER_IP = '172.21.23.10'
-OUTSIDE_PORT = 54321
-INSIDE_PORT = 12345
-DESTINATION_SERVER = '142.250.189.206' #google
-DESTINATION_PORT = 80
-DATAGRAM_SIZE = 128
+# TUNNEL_CLIENT_IP = '172.21.23.9'
+# TUNNEL_CLIENT_PORT = 12345
+# TUNNEL_SERVER_IP = '172.21.23.10'
+# OUTSIDE_PORT = 54321
+# INSIDE_PORT = 12345
+# DESTINATION_SERVER = '142.250.189.206' #google
+# DESTINATION_PORT = 80
+# DATAGRAM_SIZE = 128
 
 
 class SynchronizedDict:
@@ -43,7 +51,8 @@ class SynchronizedDict:
         with self._lock:
             return self._data.keys()
 
-def close_tcp_connection(udp_socket, shared_dict, id, client_knows=False):
+def close_tcp_connection(udp_socket, shared_dict, id, tunnel_client_ip, 
+                         tunnel_client_port, client_knows=False):
     if not shared_dict.get_value(id):
         return
     conn = shared_dict.get_value(id)
@@ -56,11 +65,12 @@ def close_tcp_connection(udp_socket, shared_dict, id, client_knows=False):
             "conn_id": id,
             "data": ''
         }
-        server_address_port = (TUNNEL_CLIENT_IP, TUNNEL_CLIENT_PORT)
+        server_address_port = (tunnel_client_ip, tunnel_client_port)
         udp_socket.sendto(json.dumps(message).encode('utf-8'), server_address_port)
         print("Wysłano wiadomość na tunel-klient:", message, "\n")
 
-def forward_tcp_connection(udp_socket, shared_dict, id):
+def forward_tcp_connection(udp_socket, shared_dict, id, tunnel_client_ip, 
+                           tunnel_client_port):
     while True:
         if not shared_dict.get_value(id):
             break
@@ -69,7 +79,8 @@ def forward_tcp_connection(udp_socket, shared_dict, id):
             break
         print("Odebrano wiadomość z serwera zewnętrznego:", data, "\n")
         if not data:
-            close_tcp_connection(udp_socket, shared_dict, id)
+            close_tcp_connection(udp_socket, shared_dict, id, tunnel_client_ip, 
+                                 tunnel_client_port)
             break
         
         message = {
@@ -77,12 +88,13 @@ def forward_tcp_connection(udp_socket, shared_dict, id):
             "conn_id": id,
             "data": data.decode('utf-8')
         }
-        server_address_port = (TUNNEL_CLIENT_IP, TUNNEL_CLIENT_PORT)
+        server_address_port = (tunnel_client_ip, tunnel_client_port)
         message = json.dumps(message).encode('utf-8')
         print("Wysłano wiadomość na tunel-klient:", message, "\n")
         udp_socket.sendto(message, server_address_port)
 
-def start_udp_server(udp_socket, shared_dict):
+def start_udp_server(udp_socket, shared_dict, destination_server, destination_port, 
+                     tunnel_client_ip, tunnel_client_port):
     while True:
         # Czekamy na pakiet UDP przychodzący od tunelu-serwera
         udp_response, ret_address = udp_socket.recvfrom(65535)
@@ -91,14 +103,16 @@ def start_udp_server(udp_socket, shared_dict):
         id = udp_response["conn_id"]
         # Klient zakończył działanie
         if MsgType(udp_response["msg_type"]) == MsgType.CONN_CLOSE_CLIENT:
-            close_tcp_connection(udp_socket, shared_dict, id, True)
+            close_tcp_connection(udp_socket, shared_dict, id, tunnel_client_ip, 
+                                 tunnel_client_port, True)
             continue
         # Odczytujemy ID Połączenia
         if not id in shared_dict.get_all_keys():
             new_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            new_tcp_socket.connect((DESTINATION_SERVER, DESTINATION_PORT))
+            new_tcp_socket.connect((destination_server, destination_port))
             shared_dict.set_value(id, (new_tcp_socket))
-            client_thread = threading.Thread(target=forward_tcp_connection, args=(udp_socket, shared_dict, id))
+            client_thread = threading.Thread(target=forward_tcp_connection, args=(udp_socket, shared_dict, id, 
+                                                                                  tunnel_client_ip, tunnel_client_port))
             client_thread.start()
         connection = shared_dict.get_value(id)
         # Przesyłamy dane na te połączenie
@@ -106,12 +120,22 @@ def start_udp_server(udp_socket, shared_dict):
             connection.sendall(udp_response["data"].encode('utf-8'))
             print("Wysłano wiadomość do serwera zewnętrznego:", udp_response["data"], "\n")
         except BrokenPipeError as e:
-            close_tcp_connection(udp_socket, shared_dict, id)
+            close_tcp_connection(udp_socket, shared_dict, id, tunnel_client_ip, tunnel_client_port)
 
 def main():
+    args = argparse.ArgumentParser()
+    args.add_argument('tunnel_client_ip')
+    args.add_argument('tunnel_client_port')
+    args.add_argument('tunnel_server_ip')
+    args.add_argument('outside_port')
+    args.add_argument('inside_port')
+    args.add_argument('destination_server')
+    args.add_argument('destination_port')
+    args.add_argument('datagram_size')
+
     shared_dict = SynchronizedDict()
 
-    udp_address_port = (TUNNEL_SERVER_IP, INSIDE_PORT)
+    udp_address_port = (args.tunnel_server_ip, args.inside_port)
 
     udp_socket = socket.socket(
         family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -119,7 +143,9 @@ def main():
 
     print(f'Tunnel Server is up!')
 
-    udp_thread = threading.Thread(target=start_udp_server, args=(udp_socket, shared_dict))
+    udp_thread = threading.Thread(target=start_udp_server, args=(udp_socket, shared_dict, args.destination_server, 
+                                                                 args.destination_port, args.tunnel_client_ip, 
+                                                                 args.tunnel_client_port))
 
     udp_thread.start()
 
